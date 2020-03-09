de0fc3091719096a073ad158b66fb649216ef3a9
Weizhen Wang <hawking.rei@gmail.com>
2020-03-09 14:14:52 +0800

    Add WAL write duration metric
    Add WAL write duration metric
    
    UCP tikv/tikv#6541
    
    Signed-off-by: Wangweizhen <hawking.rei@gmail.com>
    Signed-off-by: tabokie <xy.tao@outlook.com>
    Signed-off-by: v01dstar <yang.zhang@pingcap.com>

--- db/db_impl/db_impl_write.cc
+++ db/db_impl/db_impl_write.cc
@@ -1373,6 +1373,7 @@ IOStatus DBImpl::WriteToWAL(const WriteThread::WriteGroup& write_group,
   // Same holds for all in the batch group
   size_t write_with_wal = 0;
   WriteBatch* to_be_cached_state = nullptr;
+  StopWatch write_sw(immutable_db_options_.clock, stats_, DB_WRITE_WAL_TIME);
   WriteBatch* merged_batch;
   io_s = status_to_io_status(MergeBatch(write_group, &tmp_batch_, &merged_batch,
                                         &write_with_wal, &to_be_cached_state));
@@ -1469,6 +1470,7 @@ IOStatus DBImpl::ConcurrentWriteToWAL(
   WriteBatch tmp_batch;
   size_t write_with_wal = 0;
   WriteBatch* to_be_cached_state = nullptr;
+  StopWatch write_sw(immutable_db_options_.clock, stats_, DB_WRITE_WAL_TIME);
   WriteBatch* merged_batch;
   io_s = status_to_io_status(MergeBatch(write_group, &tmp_batch, &merged_batch,
                                         &write_with_wal, &to_be_cached_state));
